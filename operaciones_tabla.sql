f6e0ce29a5a95419b94884f5883e4a790affa2c9
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-09-16 20:13:13 -0300

    DATE was modified from DATA to DATE

--- operaciones_tabla.sql
+++ operaciones_tabla.sql
@@ -2,7 +2,7 @@ CREATE DATABASE posts;
 CREATE TABLE post(
     id SERIAL,
     nombre_usuario VARCHAR(15),
-    fecha_creacion DATA,
+    fecha_creacion DATE,
     contenido VARCHAR(200),
     descripcion VARCHAR(100),
     PRIMARY KEY (id)
@@ -25,3 +25,11 @@ INSERT INTO post(
     'description for carlos'
 );
 ALTER TABLE post ADD COLUMN titulo VARCHAR(50);
+
+UPDATE post SET titulo='title n one' WHERE id=1;
+UPDATE post SET titulo='title n two' WHERE id=2;
+UPDATE post SET titulo='title n three' WHERE id=3;
+
+
+
+
