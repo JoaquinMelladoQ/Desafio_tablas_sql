629fe49798480f65cbb5957e8e6a514bda438f29
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-09-16 19:12:51 -0300

    creating table post and setting its attributes

--- operaciones_tabla.sql
+++ operaciones_tabla.sql
@@ -1,1 +1,9 @@
 CREATE DATABASE posts;
+CREATE TABLE post(
+    id SERIAL,
+    nombre_usuario VARCHAR(15),
+    fecha_creacion DATA,
+    contenido VARCHAR(200),
+    descripcion VARCHAR(100),
+    PRIMARY KEY (id)
+);
